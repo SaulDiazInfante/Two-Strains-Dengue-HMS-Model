c7ba3902b4308513f15fa313c303a804f3b16a5f
SAUL DIAZ INFANTE VELASCO <sauld@cimat.mx>
2026-04-14 15:00:25 -0700

    Refactoring rhs ode conservation laws

--- python_code/tests/test_stochastic_search.py
+++ python_code/tests/test_stochastic_search.py
@@ -94,51 +94,13 @@ def test_compute_ode_rhs():
     S, I_1, I_2, R_s = state_host
     S_m1, Y_m1_c, Y_m1_h, R_s_m1 = state_host_m1
 
-    total_host_population = (Lambda_S + Lambda_S_m1) / mu_H
-    host_to_vector_force = beta_M * b / total_host_population
-    vector_to_host_force = beta_H * b / total_host_population
-    A_I1 = host_to_vector_force * I_1
-    A_I2 = host_to_vector_force * I_2
-    A_Y_m1_c = host_to_vector_force * Y_m1_c
-    A_Y_m1_h = host_to_vector_force * Y_m1_h
-    B_M1 = vector_to_host_force * M_1
-    B_M2 = vector_to_host_force * M_2
-    reinfection_incidence = sigma * B_M2 * S_m1
-
-    dM_s = Lambda_M - (A_I1 + A_I2 + A_Y_m1_c + A_Y_m1_h) * M_s - mu_M * M_s
-    dM_1 = A_I1 * M_s - mu_M * M_1
-    dM_2 = (A_I2 + A_Y_m1_c + A_Y_m1_h) * M_s - mu_M * M_2
-    expected_vector_conservation_law = (dM_s + dM_1 + dM_2)
-    assert abs(expected_vector_conservation_law) < 1e-8
-
-    dS = Lambda_S - (B_M1 + B_M2) * S - mu_H * S
-    dI_1 = B_M1 * S - (alpha_c + mu_H) * I_1
-    dI_2 = B_M2 * S - (alpha_c + mu_H) * I_2
-    dR_s = alpha_c * (I_1 + I_2)  - mu_H * R_s
-    expected_host_conservation_law = (dS + dI_1 + dI_2 + dR_s)
-    assert abs(expected_host_conservation_law) < 1e-8
-
-    dS_m1 = Lambda_S_m1 - (reinfection_incidence + mu_H * S_m1)
-    dY_m1_c = (1.0 - theta) * reinfection_incidence - (alpha_c + mu_H) * Y_m1_c
-    dY_m1_h = theta * reinfection_incidence - (alpha_h + mu_H) * Y_m1_h
-    dR_s_m1 = alpha_c * Y_m1_c + alpha_h * Y_m1_h  - mu_H * R_s_m1
-    expected_host_m1_conservation_law = ( dS_m1 + dY_m1_c + dY_m1_h + dR_s_m1 )
-    assert abs(expected_host_m1_conservation_law) < 1e-8
-
-    dZ = p * (I_1 + I_2 + Y_m1_c)
-
     state = np.concatenate((state_vector, state_host, state_host_m1), dtype=float)
     derivatives = StochasticSearch.compute_ode_rhs(state, 0.0, **params)
-    expected = np.array(
-        [   dM_s, dM_1, dM_2,
-            dS, dI_1, dI_2, dR_s,
-            dS_m1, dY_m1_c, dY_m1_h, dR_s_m1
-        ]
-    )
-    vector_conservation_law = np.sum(expected[0:3])
-    host_conservation_law = np.sum(expected[3:7])
-    host_m1_conservation_law= np.sum(expected[7:11])
+
+    vector_conservation_law = np.sum(derivatives[0:3])
+    host_conservation_law = np.sum(derivatives[3:7])
+    host_m1_conservation_law= np.sum(derivatives[7:11])
     assert abs(vector_conservation_law) < 1e-8
     assert abs(host_conservation_law) < 1e-8
     assert abs(host_m1_conservation_law) < 1e-8
-    assert derivatives == pytest.approx(expected)
+
